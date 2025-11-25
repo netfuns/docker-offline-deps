b9c539210a0d858dd3e5e9fb96d6608dbc96a125
Wind Wan <38069008+netfuns@users.noreply.github.com>
2025-11-25 22:06:12 +0800

    Create oraclelinux9-install.sh

---
+++ oraclelinux9-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo "Installing docker environment"
+rpm -Uvh --nodeps --force *.rpm
+systemctl start docker
+systemctl enable docker
+echo "Installed docker environment"
+#rpm -qi libseccomp > /dev/null 2>&1
+#if [[ $? -ne 0 ]];then
+#    pushd libseccomp
+#    rpm -Uvh --force *.rpm
+#    popd
+#fi
+#cd compose
+#echo "Installing docker-compose component"
+#docker load -i docker-compose.tar
+#cp docker-compose /usr/bin/
+#echo "Installed docker-compose component"
+#cd ..
+#echo "Installed completely"
